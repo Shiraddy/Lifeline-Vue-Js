4a7effb26885602094132090742dd5b536ac858e
shiraz <shirazadnan53@gmail.com>
2024-05-25 22:50:54 +0000

    removed Toast

--- src/views/PracticeView.vue
+++ src/views/PracticeView.vue
@@ -236,49 +236,8 @@ export default {
     };
   },
   methods: {
-    show() {
-      this.$toast.add({
-        severity: "info",
-        summary: "Info",
-        detail: "Message Content",
-        life: 3000,
-      });
-    },
-
     showToastBtn() {
       this.showToast = true;
-    },
-
-    confirm1(event) {
-      this.$confirm.require({
-        target: event.currentTarget,
-        message: "Are you sure you want to proceed?",
-        icon: "pi pi-exclamation-triangle",
-        rejectProps: {
-          label: "Cancel",
-          severity: "secondary",
-          outlined: true,
-        },
-        acceptProps: {
-          label: "Save",
-        },
-        accept: () => {
-          this.$toast.add({
-            severity: "info",
-            summary: "Confirmed",
-            detail: "You have accepted",
-            life: 3000,
-          });
-        },
-        reject: () => {
-          this.$toast.add({
-            severity: "error",
-            summary: "Rejected",
-            detail: "You have rejected",
-            life: 3000,
-          });
-        },
-      });
     },
   },
 };
