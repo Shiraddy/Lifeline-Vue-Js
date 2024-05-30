f08623e2d5753907fdfaacd7b8dc43aab224f782
shiraz <shirazadnan53@gmail.com>
2024-05-30 17:17:23 +0000

    Roll up error

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import vue from "@vitejs/plugin-vue";
 import Components from "unplugin-vue-components/vite";
 import { PrimeVueResolver } from "unplugin-vue-components/resolvers";
 import pages from "vite-plugin-pages";
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,12 +19,5 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, "index.html"),
-        nested: path.resolve(__dirname, "nested/index.html"),
-      },
-    },
-  },
+  
 });
